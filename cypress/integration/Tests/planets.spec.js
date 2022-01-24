9ac1c0981b3038350cd4f11329f8daa9d499c6c0
damiakintomide <damiakintomide@gmail.com>
2022-01-24 21:25:46 +0100

    fixes

--- cypress/integration/Tests/planets.spec.js
+++ cypress/integration/Tests/planets.spec.js
@@ -7,14 +7,20 @@ import {PlanetsbyID, PlanetsByIDRequest} from "../../support/Pages/PlanetsPage"
 
 context("Test service - Planets", () => {
 
-    it("Test - verify from GET", ()=>{
+    it("Test - verify Headers", ()=>{
        
         PlanetsbyID.VerifyRespHeaders();
-        PlanetsbyID.VerifyResp();
-        PlanetsbyID.VerifyRespTime();
     });
 
-  
+    it("Test - verify Response from GET", ()=>{
+       
+        PlanetsbyID.VerifyResp();
+    });
+
+    it("Test - verify Response Time", ()=>{
+       
+        PlanetsbyID.VerifyRespTime();
+    });
 
     it("Mock response to return new value", () => {
         cy.intercept({method: 'GET', url: 'api/planets/3', },
--- cypress/support/Pages/PlanetsPage.js
+++ cypress/support/Pages/PlanetsPage.js
@@ -1,67 +1,62 @@
 /// <reference types ="Cypress" />
 
 export class PlanetsByIDRequest {
-    ReadData(){
-       cy.fixture("planets").as("planets");
-       
-    }
-
+  
 
     VerifyRespHeaders(){
-        PlanetsbyID.ReadData();
-        cy.get("@planets").then((planets) =>{
-            cy.request(planets.endpointplantid+"3").then((response) => {
-                expect(response).has.property("headers");
-                expect(response.headers).to.include({'allow': "GET, HEAD, OPTIONS",
-                'connection': "keep-alive",
-                'content-type': "application/json"});
-                expect(response.status).to.eq(200);
-               
-            });
+      
+        cy.request("api/planets/3").then((response) => {
+            expect(response).has.property("headers");
+            expect(response.headers).to.include({'allow': "GET, HEAD, OPTIONS",
+            'connection': "keep-alive",
+            'content-type': "application/json"});
+            expect(response.status).to.eq(200);
+            
         });
+        
        
      
     }
 
     VerifyResp(){ 
-        PlanetsbyID.ReadData();
-        cy.get("@planets").then((planets) =>{
-            cy.request(planets.endpointplantid+"3").then((response) => {
-            
-                expect(response.body).has.property("name","Yavin IV");
-                expect(response.body.property).has.value();
-            
-            });
+      
+        cy.request("api/planets/3").then((response) => {
+        
+            expect(response.body).has.property("name","Yavin IV");
+        
         });
+      
    
     }
 
     VerifyRespTime(){
        
-        PlanetsbyID.ReadData();
-        cy.get("@planets").then((planets) =>{
-        cy.request(planets.endpointplantid+"3").then((response) => {
+      
+        cy.request("api/planets/3").then((response) => {
          
             expect(response.duration/1000).lessThan(3);
         });
-        });
+       
      
     }
 
     VerifyRespPOST(){
-        PlanetsbyID.ReadData();
-        cy.get("@planets").then((planets) =>{
+      
             cy.request({
                 method:'POST',
-                url: planets+endpointplantid+"3",
-                body:planets.planetbody,
+                url: "api/planets/3",
+                body:  {
+                    "id": 2,
+                    "title": "Executeautomation",
+                    "author": "KarthikKK"
+                  },
                 failOnStatusCode: false
             }).then((res) =>{
             expect(res.status).to.eq(405);
             expect(res.body).has.property("detail","Method 'POST' not allowed.");
             });
        
-        });
+        
     }
    
 }
